c9db351451b51019b1a3cf9b530c40617685cc06
Emily Holtzman <Emily@admins-MacBook-Pro.local>
2024-01-09 00:31:00 -0500

    adding new lines

--- highnote.js
+++ highnote.js
@@ -1,8 +1,6 @@
 const Spotify = require('node-spotify-api');
 const readline = require('readline');
-const {
-    exec
-} = require('child_process');
+const { exec } = require('child_process');
 
 const api = new Spotify({
     id: "6e6655dfaf7e42a799ae941f92940378",
@@ -11,18 +9,20 @@ const api = new Spotify({
 
 const songName = process.argv.slice(2).join(" ")
 
-api.search({
-    type: 'track',
-    query: songName
-}, function(error, data) {
+console.log("Searching for best match...");
+
+api.search({ type: 'track', query: songName}, function (error, data) {
     if (error) {
         return console.error(error)
     }
     const topResult = data.tracks.items[0];
-    const artistName = topResult.artists[0].name;
+    const artistName = 
+    topResult.artists[0].name;
     const url = topResult.external_urls.spotify;
     const releaseDate = new Date(topResult.album.release_date);
+    console.log("\n");
     console.log(`The top search result for "${songName}" is the song "${topResult.name}", released by the artist "${artistName}" on ${releaseDate.toDateString()}."`);
+    console.log("\n");
     const rl = readline.createInterface({
         input: process.stdin,
         output: process.stdout
@@ -34,5 +34,5 @@ api.search({
             console.log('whatever bro...')
         }
         rl.close();
-    });
+      });
 });
